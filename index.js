18296e1f45f01f7d628d09942adbf7618af537cc
David Furnes <david@dfurnes.com>
2016-02-05 16:52:29 -0500

    Simple example with reversing things.

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 require('dotenv').config();
 
-
 var Slack = require('slack-client');
 
 slack = new Slack(process.env.SLACK_TOKEN, true, true);
 
-// SLACK IT TI KCALS
-require('./simple_reverse')(slack);
+/**
+ * When joining Slack...
+ */
+slack.on('open', function() {
+  console.log('Welcome to Slack. You are @' + slack.self.name + ' of ' + slack.team.name + '.');
+});
 
+/**
+ * On receiving a message...
+ */
+slack.on('message', function(message) {
+  var channel = slack.getChannelGroupOrDMByID(message.channel);
+  var user = slack.getUserByID(message.user);
+
+  var username = '???';
+  if(user && user.name) {
+    username = '@' + user.name;
+  }
+
+  console.log('[RECEIVED] ' + username + ': ' + message.text);
+
+  var response = message.text.split('').reverse().join('');
+  channel.send(response)
+});
+
+/**
+ * Handle errors...
+ */
 slack.on('error', function(error) {
   return console.error("Error: " + error);
 });
