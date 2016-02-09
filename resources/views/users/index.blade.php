b71924c884c7aaa02cabd8af9893e5b13b1efdce
katiecrane <katiethecrane@gmail.com>
2016-02-09 12:14:21 -0500

    make user index page more readable

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,13 +1,21 @@
 <!-- list all the users here, this should probably be under admin/users
 (and then displays only non-admins) -->
 
+<h1>Users</h1>
 
-<tbody>
+<table>
+	<tr>
+		<th>User ID</th>
+		<th>Name</th>
+		<th>Slack Name</th>
+		<th>Start Date</th>
+	</tr>
   @foreach($users as $user)
     <tr>
-      <td>{{ $user->id  }}</td>
-      <td>{{ $user->name  }}</td>
-      <td> {{{ $user->slack_name }}} </td>
-      <td> {{{ $user->start_date }}} </td>
+      <td>{{ $user->id }}</td>
+      <td>{{ $user->name }}</td>
+      <td> {{ $user->slack_name }} </td>
+      <td> {{ $user->start_date }} </td>
+    </tr>
   @endforeach
-</tbody>
+</table>
