d2acf90f91479820d9348fae7ca6dc05ade4c1d4
Andrea Gaither <angaither@gmail.com>
2016-02-11 10:41:59 -0500

    Get the message for the current day the user is on.

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -27,7 +27,10 @@ class Message extends Model
     /**
      *
      */
-    public static function getMessage() {
-      return self::first();
+    public static function getMessage(User $user) {
+        $today = new \DateTime();
+        $start_date = new \DateTime($user->start_date);
+        $currentOnboardingDay = $today->diff($start_date);
+        return self::where('day_to_send', '=', $currentOnboardingDay->days)->first();
     }
 }
