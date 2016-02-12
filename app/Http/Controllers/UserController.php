16c991ffbe4414704625f223589f2be502a807c2
Shae Smith <ssmith@dosomething.org>
2016-02-12 11:15:03 -0500

    add comments

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,21 +6,35 @@ use Request;
 use Toto\Http\Controllers\Controller;
 use Toto\Models\User;
 
-class UserController extends Controller 
+class UserController extends Controller
 {
 
+    /**
+     * Display a list of users.
+     *
+     * @return Response
+     */
 	public function index()
 	{
 	    $users = User::all();
 		return view('users.index', compact('users'));
 	}
 
+    /**
+     * Show the form for creating a new user.
+     *
+     * @return Response
+     */
 	public function create()
 	{
 		return view('users.create');
 	}
 
-
+    /**
+     * Store a newly created user.
+     *
+     * @return Response
+     */
 	public function store()
 	{
 		$input = Request::all();
@@ -28,6 +42,12 @@ class UserController extends Controller
 		return redirect('users');
 	}
 
+    /**
+     * Display the specified user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
 	public function show($id)
 	{
 	    // Eventually need to deal with what to do if the user is not found
@@ -35,12 +55,24 @@ class UserController extends Controller
 		return view('users.show', compact('user'));
 	}
 
+    /**
+     * Show the form for editing a user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
 	public function edit($id)
 	{
 		$user = User::find($id);
 		return view('users.edit', compact('user'));
 	}
 
+    /**
+     * Update the specified user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
 	public function update($id)
 	{
 		$user = User::find($id);
@@ -51,4 +83,21 @@ class UserController extends Controller
 		return redirect('users');
 
 	}
+
+    /**
+     * Remove the user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id, Request $request)
+    {
+        $message = Message::findOrFail($id);
+
+        $message->delete();
+
+        $request->session()->flash('status', 'Message has been deleted!');
+
+        return redirect()->route('messages.index');
+    }
 }
