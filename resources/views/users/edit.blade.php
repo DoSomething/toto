1f27636909ccaf341f8f040dbbc402bc1822931f
Shae Smith <ssmith@dosomething.org>
2016-02-11 23:47:29 -0500

    Show user styles

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,24 +1,41 @@
-<p>U R now editin': {{ $user->name }}</p>
+@extends('layouts.master')
 
-{!! Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user->id]]) !!}
+@section('title', 'Edit '. $user->name)
 
-	<div>
-		{!! Form::label('name', 'Name: ') !!}
-		{!! Form::input('text', 'name', null) !!}
-	</div>
+@section('content')
+<div class='container__block'>
+    <div class='wrapper'>
+        <p>Edit this user below. <a href="{{ route('messages.index') }}">Go back to all users.</a></p>
+    </div>
+</div>
+<div class='container__block'>
+    <div class='wrapper'>
+        {!! Form::model($user, ['method' => 'PATCH', 'route' => ['users.update', $user->id]]) !!}
 
-	<div>
-		{!! Form::label('slack_name', 'Slack Name: @') !!}
-		{!! Form::input('text', 'slack_name', null, ['placeholder' => 'whatever']) !!}
-	</div>
+            <div class="form-item">
+                {!! Form::label('name', 'Name: ', array('class' => 'field-label')) !!}
+                {!! Form::text('name', NULL, array('class' => 'text-field')) !!}
+            </div>
 
-	<div>
-		{!! Form::label('start_date', 'Start Date: ') !!}
-		{!! Form::input('date', 'start_date', null, ['placeholder' => 'MM/DD/YYYY']) !!}
-	</div>
+            <div class="form-item">
+                {!! Form::label('slack_name', 'Slack Name: (@)', array('class' => 'field-label')) !!}
+                <div class="user__slack_field">
+                    {!! Form::text('slack_name', NULL, array('class' => 'text-field')) !!}
+                </div>
+            </div>
 
-	<div>
-		{!! Form::submit('Update User') !!}
-	</div>
+            <div class="form-item">
+                {!! Form::label('start_date', 'Start Date: ', array('class' => 'field-label')) !!}
+                {!! Form::text('start_date', NULL, ['class' => 'text-field', 'placeholder' => 'MM/DD/YYYY']) !!}
 
-{!! Form::close() !!}
+            </div>
+
+            <div class="form-item">
+                {!! Form::submit('Update User',  ['class' => 'button']) !!}
+            </div>
+
+        {!! Form::close() !!}
+    </div>
+</div>
+
+@stop
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,4 +1,4 @@
-<h1>User: {{ $user->name }}</h1>
+<!-- <h1>User: {{ $user->name }}</h1>
 
 <div>
     <p><strong>User ID: </strong> {{ $user->id }}</p>
@@ -16,3 +16,34 @@
 <div>
     <a href="{{ route('users.index') }}">&larr; Return to all users</a>
 </div>
+ -->
+@extends('layouts.master')
+
+@section('title', $user->name)
+
+@section('content')
+
+<ul>
+    <li class='container__block'>
+        <h2 class="heading -beta"><span>User info</span></h2>
+    </li>
+    <li class='container__block'>
+        <p><strong>User ID: </strong> {{ $user->id }}</p>
+        <p><strong>Name: </strong>{{ $user->name }}</p>
+        <p><strong>Slack Name: </strong>{{ $user->slack_name }}</p>
+        <p><strong>Start Date: </strong>{{ $user->start_date }}</p>
+    </li>
+</ul>
+
+<ul class="form-actions -inline">
+    <li>
+        {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id]]) !!}
+        {!! Form::submit('Delete', array('class' => 'button delete')) !!}
+        {!! Form::close() !!}
+    </li>
+    <li>
+        <a href="{{ route('users.edit', $user->id) }}" class="button">Edit user</a>
+    </li>
+</ul>
+
+@stop
