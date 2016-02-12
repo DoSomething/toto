645355ea208fc64301e8c7bcd907525e13406efb
Shae Smith <ssmith@dosomething.org>
2016-02-11 21:02:37 -0500

    index view styles

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,22 +1,7 @@
 <?php
 
 /*
- * Message CRUD routes
+ * CRUD routes
  */
 Route::resource('messages', 'MessageController');
 Route::resource('users', 'UserController');
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-Route::group(['middleware' => ['web']], function () {
-    //
-});
